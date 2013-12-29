71bc968e2a5503726f4e0fff09938cbd97dcc464
Ryan Slade <ryanslade@gmail.com>
2013-12-29 14:05:05 +0000

    Slight refactor

--- server/server.go
+++ server/server.go
@@ -8,53 +8,79 @@ import (
 	"net"
 )
 
-const (
-	OpRRQ   = 1
-	OpWRQ   = 2
-	OpDATA  = 3
-	OpACK   = 4
-	OpERROR = 5
+type OpCode uint16
 
+const (
+	OpRRQ   OpCode = 1
+	OpWRQ   OpCode = 2
+	OpDATA  OpCode = 3
+	OpACK   OpCode = 4
+	OpERROR OpCode = 5
+)
+
+const (
 	maxPacketSize = 2048
 )
 
-func listen(addr *net.UDPAddr) error {
+type RequestPacket struct {
+	OpCode   OpCode
+	FileName string
+	Mode     string
+}
+
+func getOpCode(packet []byte) (OpCode, error) {
+	if len(packet) < 2 {
+		return OpERROR, fmt.Errorf("Packet too small to get opcode")
+	}
+	opcode := binary.BigEndian.Uint16(packet)
+	if opcode > 5 {
+		return OpERROR, fmt.Errorf("Unknown opcode: %d", opcode)
+	}
+	return OpCode(opcode), nil
+}
+
+func waitForRequest(addr *net.UDPAddr) (*RequestPacket, *net.UDPAddr, error) {
 	conn, err := net.ListenUDP("udp", addr)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	defer conn.Close()
 
 	packet := make([]byte, maxPacketSize)
 	n, remoteAddr, err := conn.ReadFromUDP(packet)
 	if err != nil {
-		return err
+		return nil, nil, err
 	}
 	if n == maxPacketSize {
-		return fmt.Errorf("Packet too big: %d bytes", n)
+		return nil, nil, fmt.Errorf("Packet too big: %d bytes", n)
 	}
-	fmt.Println(remoteAddr)
 
 	// Get opcode
-	opcode := binary.BigEndian.Uint16(packet)
-	log.Println("Opcode:", opcode)
+	opcode, err := getOpCode(packet)
+	if err != nil {
+		return nil, remoteAddr, err
+	}
 
 	// Get filename
 	reader := bytes.NewBuffer(packet[2:])
 	filename, err := reader.ReadBytes(byte(0))
 	if err != nil {
-		return fmt.Errorf("Error reading filename: %v", err)
+		return nil, remoteAddr, fmt.Errorf("Error reading filename: %v", err)
 	}
-	log.Println("Filename:", string(filename))
 
 	// Get mode
 	mode, err := reader.ReadBytes(byte(0))
 	if err != nil {
-		return fmt.Errorf("Error reading mode: %v", err)
+		return nil, remoteAddr, fmt.Errorf("Error reading mode: %v", err)
 	}
-	log.Println("Mode:", string(mode))
 
-	return nil
+	request := &RequestPacket{
+		OpCode:   opcode,
+		FileName: string(filename),
+		Mode:     string(mode),
+	}
+
+	return request, remoteAddr, nil
 }
 
 func main() {
@@ -64,10 +90,12 @@ func main() {
 		return
 	}
 	for {
-		log.Println("Listening")
-		err := listen(addr)
+		log.Println("Waiting for request")
+		request, remoteAddr, err := waitForRequest(addr)
 		if err != nil {
 			log.Println(err)
+			continue
 		}
+		log.Printf("Request: %+v from %s", request, remoteAddr.String())
 	}
 }
