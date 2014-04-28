fbf375fec5d37bafd87c42ff9815c67f03378c5c
Ryan Slade <ryanslade@gmail.com>
2014-04-28 21:23:01 +0100

    Added tests for GetOpcode

--- server/server_test.go
+++ server/server_test.go
@@ -18,3 +18,57 @@ func TestCreateErrorPacket(t *testing.T) {
 		t.Errorf("%v", p)
 	}
 }
+
+func TestGetOpcode(t *testing.T) {
+	testCases := []struct {
+		data           []byte
+		expectedOpcode OpCode
+		shouldError    bool
+	}{
+		// Standard RRQ
+		{
+			data:           []byte{0, 1},
+			expectedOpcode: OpRRQ,
+			shouldError:    false,
+		},
+		// Empty data
+		{
+			data:           []byte{},
+			expectedOpcode: OpERROR,
+			shouldError:    true,
+		},
+		// Unknown opcode
+		{
+			data:           []byte{0, 99},
+			expectedOpcode: OpERROR,
+			shouldError:    true,
+		},
+		// Only 1 byte
+		{
+			data:           []byte{1},
+			expectedOpcode: OpERROR,
+			shouldError:    true,
+		},
+		// More than 2 bytes
+		{
+			data:           []byte{0, 1, 2},
+			expectedOpcode: OpRRQ,
+			shouldError:    false,
+		},
+	}
+
+	for i, tc := range testCases {
+		oc, err := getOpCode(tc.data)
+		if tc.shouldError && err == nil {
+			t.Errorf("Expected error, didn't get one (%d)", i)
+			continue
+		}
+		if !tc.shouldError && err != nil {
+			t.Errorf("%v (%d)", err)
+			continue
+		}
+		if oc != tc.expectedOpcode {
+			t.Errorf("Expected: %v, got %v (%d)", tc.expectedOpcode, oc, i)
+		}
+	}
+}
