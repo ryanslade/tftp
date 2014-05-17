bbacae5ffcecc68445eaf53393791a1bfa8087f2
Ryan Slade <ryanslade@gmail.com>
2014-05-17 10:15:47 +0100

    Moved mocks into their own file

--- server/server_test.go
+++ server/server_test.go
@@ -2,65 +2,14 @@ package main
 
 import (
 	"bytes"
-	"io"
 	"io/ioutil"
 	"log"
-	"net"
 	"reflect"
 	"testing"
-	"time"
 )
 
 func init() {
 	log.SetOutput(ioutil.Discard)
-}
-
-type mockAddr struct{}
-
-func (m mockAddr) Network() string {
-	return "udp"
-}
-
-func (m mockAddr) String() string {
-	return "mockAddr"
-}
-
-type mockPacketConn struct {
-	data *bytes.Buffer
-	addr net.Addr
-}
-
-func (m *mockPacketConn) ReadFrom(b []byte) (n int, add net.Addr, err error) {
-	to := bytes.NewBuffer(b)
-	to.Truncate(0)
-	n64, err := io.Copy(to, m.data)
-	return int(n64), m.addr, err
-}
-
-func (m *mockPacketConn) WriteTo(b []byte, add net.Addr) (n int, err error) {
-	from := bytes.NewReader(b)
-	n64, err := io.Copy(m.data, from)
-	return int(n64), err
-}
-
-func (m *mockPacketConn) Close() error {
-	return nil
-}
-
-func (m *mockPacketConn) LocalAddr() net.Addr {
-	return m.addr
-}
-
-func (m *mockPacketConn) SetDeadline(t time.Time) error {
-	return nil
-}
-
-func (m *mockPacketConn) SetReadDeadline(t time.Time) error {
-	return nil
-}
-
-func (m *mockPacketConn) SetWriteDeadline(t time.Time) error {
-	return nil
 }
 
 func sampleRRQ() []byte {
