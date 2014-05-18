71724422c56ddd72acfd53dbb5dc7301ddb3cc47
Ryan Slade <ryanslade@gmail.com>
2014-05-18 20:39:54 +0100

    Created a test for createDataPacket

--- server/server_test.go
+++ server/server_test.go
@@ -14,6 +14,28 @@ func init() {
 	handlerMapping = map[OpCode]requestHandler{}
 }
 
+func TestCreateDataPacket(t *testing.T) {
+	testCases := []struct {
+		blockNumber uint16
+		data        []byte
+		expected    []byte
+	}{
+		{
+			blockNumber: 1,
+			data:        []byte{1, 2, 3, 4, 5},
+			expected:    []byte{0, 3, 0, 1, 1, 2, 3, 4, 5},
+		},
+	}
+
+	for i, tc := range testCases {
+		packet := createDataPacket(tc.blockNumber, tc.data)
+		if !reflect.DeepEqual(packet, tc.expected) {
+			t.Errorf("Expected and actual packet not equal (%d)", i)
+			t.Error(packet)
+		}
+	}
+}
+
 func TestParseACKPacket(t *testing.T) {
 	testCases := []struct {
 		packet      []byte
